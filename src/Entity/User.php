eba2eabc761f0d907b908b4ba6b4280559a13942
Maxime Frémeaux <maxime.fremeaux2@gmail.com>
2024-09-13 02:01:12 +0200

    add admin middleware

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -144,4 +144,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function hasRole(string $role): bool
+    {
+        return $this->roles->contains($role);
+    }
 }
